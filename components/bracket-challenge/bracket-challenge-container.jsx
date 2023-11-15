8ff6e43d9c95a5643c02e34bda7ccb1e33d92787
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-11-15 01:39:21 -0500

    gender buttons

--- components/bracket-challenge/bracket-challenge-container.jsx
+++ components/bracket-challenge/bracket-challenge-container.jsx
@@ -8,6 +8,7 @@ import { ROUNDS } from '@/utils/constants';
 import { MatchupDataProvider } from '@/context/matchup-context/matchup-context';
 import BracketChallenge from './bracket-challenge';
 import { useSnowboarders } from '@/context/snowboarders-context/snowboarders-context';
+import GenderButtons, { GENDERS } from './gender-buttons/gender-buttons';
 
 export default function BracketChallengeContainer() {
   const [isLoading, setIsLoading] = useState(false);
@@ -15,6 +16,7 @@ export default function BracketChallengeContainer() {
   const currentRoundName = currentRound.name;
   const [isSettingName, setIsSettingName] = useState(true);
   const snowboarders = useSnowboarders();
+  const [gender, setGender] = useState('male');
 
   const router = useRouter();
   useEffect(() => {
@@ -32,6 +34,7 @@ export default function BracketChallengeContainer() {
     Selkirk: 'eightMulti',
     SelkirkWomen: 'fourMulti',
   };
+
   return (
     <>
       {isSettingName ? (
@@ -45,28 +48,32 @@ export default function BracketChallengeContainer() {
             setIsLoading={setIsLoading}
             classNames={styles.roundButtons}
           />
+          <GenderButtons setGender={setGender} setIsLoading={setIsLoading} />
           {isLoading ? (
             <Loader classNames={styles.loader} />
           ) : (
             <>
-              <MatchupDataProvider
-                snowboarders={snowboarders[currentRoundName]}
-              >
-                <BracketChallenge
-                  currentRound={currentRoundName}
-                  bracketConfig={mapRoundToBracketConfig[currentRoundName]}
-                />
-              </MatchupDataProvider>
-              <MatchupDataProvider
-                snowboarders={snowboarders[currentWomensRoundName]}
-              >
-                <BracketChallenge
-                  currentRound={currentWomensRoundName}
-                  bracketConfig={
-                    mapRoundToBracketConfig[currentWomensRoundName]
-                  }
-                />
-              </MatchupDataProvider>
+              {gender === GENDERS.MALE ? (
+                <MatchupDataProvider
+                  snowboarders={snowboarders[currentRoundName]}
+                >
+                  <BracketChallenge
+                    currentRound={currentRoundName}
+                    bracketConfig={mapRoundToBracketConfig[currentRoundName]}
+                  />
+                </MatchupDataProvider>
+              ) : (
+                <MatchupDataProvider
+                  snowboarders={snowboarders[currentWomensRoundName]}
+                >
+                  <BracketChallenge
+                    currentRound={currentWomensRoundName}
+                    bracketConfig={
+                      mapRoundToBracketConfig[currentWomensRoundName]
+                    }
+                  />
+                </MatchupDataProvider>
+              )}
             </>
           )}
         </>
---
+++ components/bracket-challenge/gender-buttons/gender-buttons.jsx
@@ -0,0 +1,29 @@
+import Button from '@/components/shared/button/button';
+import styles from './gender-buttons.module.scss';
+
+export const GENDERS = {
+  MALE: 'male',
+  FEMALE: 'female',
+};
+
+export default function GenderButtons({ setGender, setIsLoading }) {
+  const handleSetGender = ({ gender }) => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setGender(gender);
+    }, 200);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 300);
+  };
+  return (
+    <div className={styles.genderButtons}>
+      <Button handleClick={() => handleSetGender({ gender: GENDERS.MALE })}>
+        Men
+      </Button>
+      <Button handleClick={() => handleSetGender({ gender: GENDERS.FEMALE })}>
+        Women
+      </Button>
+    </div>
+  );
+}
