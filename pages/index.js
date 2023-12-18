7b9af640d15d8a172b7a828d6e48ef17b2d3b8e0
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-12-18 00:35:24 -0500

    coming soon page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import ComingSoon from '@/components/coming-soon/coming-soon';
 import Login from 'components/login/login';
 import { useUser } from 'context/user-context/user-context';
 import Cookies from 'js-cookie';
@@ -5,18 +6,24 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { COOKIES, ROUTES } from 'utils/constants';
 
+// export default function Home() {
+//   const { authData } = useUser();
+//   const router = useRouter();
+//   useEffect(() => {
+//     if (authData) {
+//       const authCookie = Cookies.get(COOKIES.UID);
+//       if (authCookie) {
+//         router.push({
+//           pathname: ROUTES.DASHBOARD,
+//         });
+//       }
+//     }
+//   }, [authData, router]);
+//   return <Login />;
+// }
+
 export default function Home() {
-  const { authData } = useUser();
-  const router = useRouter();
-  useEffect(() => {
-    if (authData) {
-      const authCookie = Cookies.get(COOKIES.UID);
-      if (authCookie) {
-        router.push({
-          pathname: ROUTES.DASHBOARD,
-        });
-      }
-    }
-  }, [authData, router]);
-  return <Login />;
+  return (
+    <ComingSoon />
+  )
 }
