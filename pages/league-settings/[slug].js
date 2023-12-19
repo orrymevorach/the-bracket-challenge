f57939713e33b913db8c6e7dd9cfad72a9f07437
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-12-19 11:10:06 -0500

    fix league settings slug

--- pages/league-settings/[slug].js
+++ pages/league-settings/[slug].js
@@ -1,6 +1,7 @@
 import LeagueSettings from '@/components/league-settings/league-settings';
 import Meta from '@/components/shared/head/head';
 import { LeagueConfigProvider } from '@/context/league-config-context/league-config-context';
+import { getLeagueIds } from '@/lib/airtable';
 import { getPageLoadData } from '@/lib/contentful';
 import { ROUTES } from '@/utils/constants';
 
@@ -24,5 +25,14 @@ export async function getStaticProps() {
     props: {
       ...pageLoadData,
     },
+  };
+}
+
+export async function getStaticPaths() {
+  const leagues = await getLeagueIds();
+
+  return {
+    paths: leagues.map(({ id }) => `/league-settings/${id}`),
+    fallback: true,
   };
 }
