5b1da4a79b8a2f31e3c180260add1cd85cf72d2e
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-02-16 13:27:08 -0500

    trying something

--- components/dashboard/join-league-takeover/join-league-takeover.jsx
+++ components/dashboard/join-league-takeover/join-league-takeover.jsx
@@ -18,7 +18,7 @@ export default function JoinLeagueTakeover({ setShowTakeover }) {
       memberRecordIds,
     });
     console.log('response.id', response.id);
-    // window.location = `${ROUTES.LEAGUE}/${response.id}`;
+    window.location = `${ROUTES.LEAGUE}/${response.id}`;
   };
 
   return (
