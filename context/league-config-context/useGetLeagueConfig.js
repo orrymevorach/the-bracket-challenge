9a287eda9fe51fd6bb8acecc33ab1ec36d6f615c
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-02-16 15:44:25 -0500

    trying something

--- context/league-config-context/useGetLeagueConfig.js
+++ context/league-config-context/useGetLeagueConfig.js
@@ -7,17 +7,15 @@ export default function useGetLeagueConfig() {
   const {
     query: { slug },
   } = useRouter();
-  console.log('slug', slug);
-  // useEffect(() => {
-  // const getLeagueConfigData = async () => {
-  // const leagueConfig = await getLeagueConfig({ id: slug });
-  // console.log('leagueConfig', leagueConfig);
-  // setLeagueConfig(leagueConfig);
-  // };
-  // if (slug && !leagueConfig) {
-  //   getLeagueConfigData();
-  // }
-  // }, [leagueConfig, slug]);
+  useEffect(() => {
+    const getLeagueConfigData = async () => {
+      const leagueConfig = await getLeagueConfig({ id: slug });
+      setLeagueConfig(leagueConfig);
+    };
+    if (slug && !leagueConfig) {
+      getLeagueConfigData();
+    }
+  }, [leagueConfig, slug]);
 
   return leagueConfig;
 }
