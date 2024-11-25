09faaa01cf41b300a266c1ff0a2be1357a76e658
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-11-24 19:36:41 -0500

    logo

--- components/HomePage/Nav/Nav.jsx
+++ components/HomePage/Nav/Nav.jsx
@@ -5,6 +5,10 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 import Button from '@/components/shared/button/button';
 import { ROUTES } from '@/utils/constants';
 import { useUser } from '@/context/user-context/user-context';
+import Image from 'next/image';
+import desktopLogo from '@/public/logo-center.png';
+import mobileLogo from '@/public/logo-bracket.png';
+import useWindowSize from '@/hooks/useWindowSize';
 
 const getLinks = ({ user }) => [
   // {
@@ -25,9 +29,12 @@ export default function Nav() {
   const user = useUser();
   const loginHref = user?.id ? ROUTES.DASHBOARD : ROUTES.LOGIN;
   const links = getLinks({ user });
+  const { isMobile } = useWindowSize();
+  const logo = isMobile ? mobileLogo : desktopLogo;
   return (
     <nav className={styles.nav}>
-      <div></div> {/* This is a placeholder for the logo */}
+      <Image src={logo} alt="logo" className={styles.logo} />
+      {/* This is a placeholder for the logo */}
       {/* <ul className={styles.items}> */}
       <ul>
         {/* {links.map(({ label, href }) => {
