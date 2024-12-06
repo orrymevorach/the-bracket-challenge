5e63c085ad1104bb7550c15fb218f2dcf7e1e5c7
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-12-06 15:19:36 -0500

    add console log

--- pages/bracket-challenge/[slug].js
+++ pages/bracket-challenge/[slug].js
@@ -9,17 +9,19 @@ import {
 } from '@/lib/airtable';
 import { createPlaceholdersForFutureRounds } from '@/context/matchup-context/matchup-utils';
 
-export default function BracketChallengePage({
-  contests = [],
-  snowboarders = {},
-  contestsInCurrentSport,
-  contestsWithAllMatchups,
-  snowboardersAsMap,
-}) {
-  console.log('contests', contests);
-  console.log('contestsInCurrentSport', contestsInCurrentSport);
-  console.log('contestsWithAllMatchups', contestsWithAllMatchups);
-  console.log('snowboardersAsMap', snowboardersAsMap);
+export default function BracketChallengePage(
+  {
+    // contests = [],
+    // snowboarders = {},
+    // contestsInCurrentSport,
+    // contestsWithAllMatchups,
+  }
+) {
+  // console.log('contests', contests);
+  // console.log('contestsInCurrentSport', contestsInCurrentSport);
+  // console.log('contestsWithAllMatchups', contestsWithAllMatchups);
+  // console.log('snowboarders', snowboarders);
+  return;
   return (
     <>
       <Meta title="Bracket Challenge" />
@@ -42,6 +44,9 @@ export async function getStaticProps(context) {
     if (!contest.sport) return false;
     return contest.sport[0].toLowerCase() === context.params.slug;
   });
+  return {
+    props: { slug: context.params.slug },
+  };
   const contestsWithAllMatchups = contestsInCurrentSport.map(contest => {
     const matchups = contest.matchups;
     if (!matchups?.length) return contest;
@@ -65,7 +70,6 @@ export async function getStaticProps(context) {
       snowboarders: snowboardersAsMap,
       contestsInCurrentSport,
       contestsWithAllMatchups,
-      snowboardersAsMap,
     },
   };
 }
