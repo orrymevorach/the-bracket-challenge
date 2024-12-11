57adb37ef2601017f88b4effc3c7e4045f73331b
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2024-12-10 22:50:15 -0500

    static props on bracket challenge

--- pages/bracket-challenge/[slug].js
+++ pages/bracket-challenge/[slug].js
@@ -27,60 +27,60 @@ export default function BracketChallengePage({ contests = [], snowboarders }) {
   );
 }
 
-// export async function getStaticProps(context) {
-//   const sport = context.params.slug;
+export async function getStaticProps(context) {
+  const sport = context.params.slug;
 
-//   const contests = await getContestsBySport({ sport });
-//   if (!contests?.length)
-//     return {
-//       props: {},
-//     };
+  const contests = await getContestsBySport({ sport });
+  if (!contests?.length)
+    return {
+      props: {},
+    };
 
-//   const { snowboarders } = await getSnowboardersBySport({ sport });
+  const { snowboarders } = await getSnowboardersBySport({ sport });
 
-//   const contestsWithSelectedWinnersAndMatchups =
-//     await populateContestsWithSelectedWinnersAndMatchups(
-//       contests,
-//       snowboarders
-//     );
+  const contestsWithSelectedWinnersAndMatchups =
+    await populateContestsWithSelectedWinnersAndMatchups(
+      contests,
+      snowboarders
+    );
 
-//   const contestsWithAllMatchups = contestsWithSelectedWinnersAndMatchups.map(
-//     contest => {
-//       const matchups = contest.matchups;
-//       if (!matchups?.length) return contest;
-//       const matchupsWithExistingDataAndPlaceholdersForFutureRounds =
-//         createPlaceholdersForFutureRounds(matchups);
-//       return {
-//         ...contest,
-//         matchups: matchupsWithExistingDataAndPlaceholdersForFutureRounds,
-//       };
-//     }
-//   );
+  const contestsWithAllMatchups = contestsWithSelectedWinnersAndMatchups.map(
+    contest => {
+      const matchups = contest.matchups;
+      if (!matchups?.length) return contest;
+      const matchupsWithExistingDataAndPlaceholdersForFutureRounds =
+        createPlaceholdersForFutureRounds(matchups);
+      return {
+        ...contest,
+        matchups: matchupsWithExistingDataAndPlaceholdersForFutureRounds,
+      };
+    }
+  );
 
-//   const snowboardersAsMap = snowboarders.reduce((acc, snowboarder) => {
-//     acc[snowboarder.name] = snowboarder;
-//     return acc;
-//   }, {});
+  const snowboardersAsMap = snowboarders.reduce((acc, snowboarder) => {
+    acc[snowboarder.name] = snowboarder;
+    return acc;
+  }, {});
 
-//   const sortedContests = contestsWithAllMatchups.sort((a, b) => {
-//     if (a.order < b.order) return -1;
-//     return 1;
-//   });
-//   return {
-//     props: {
-//       contests: sortedContests,
-//       snowboarders: snowboardersAsMap,
-//     },
-//   };
-// }
+  const sortedContests = contestsWithAllMatchups.sort((a, b) => {
+    if (a.order < b.order) return -1;
+    return 1;
+  });
+  return {
+    props: {
+      contests: sortedContests,
+      snowboarders: snowboardersAsMap,
+    },
+  };
+}
 
-// export async function getStaticPaths() {
-//   const sports = await getSports();
+export async function getStaticPaths() {
+  const sports = await getSports();
 
-//   return {
-//     paths: sports.map(
-//       sport => `/bracket-challenge/${sport.name.toLowerCase()}`
-//     ),
-//     fallback: true,
-//   };
-// }
+  return {
+    paths: sports.map(
+      sport => `/bracket-challenge/${sport.name.toLowerCase()}`
+    ),
+    fallback: true,
+  };
+}
