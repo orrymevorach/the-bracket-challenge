1cff7f108aae27c2a3a5103b07bff1cf43a62d7d
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-01-14 23:15:10 -0500

    slug fix

--- pages/bracket-challenge/[slug].js
+++ pages/bracket-challenge/[slug].js
@@ -97,9 +97,9 @@ export async function getStaticProps(context) {
 export async function getStaticPaths() {
   const sports = await getSports();
   const filteredSports = sports.filter(sport => {
-    if (!!sport?.contests?.length) return true;
-    if (sport.status === 'Open') return true;
-    return false;
+    if (!sport?.contests?.length) return false;
+    if (sport.status !== 'Open') return false;
+    return true;
   });
 
   return {
